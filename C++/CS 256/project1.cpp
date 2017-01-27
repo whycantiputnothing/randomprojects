1d811cd687e10fcc5fd5b90470f37d9c45b1eb85
Brandon Nguyen <happytreefriends001@yahoo.com>
2017-01-26 19:19:20 -0800

    cs 256

---
+++ C++/CS 256/project1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+void pause( int milliseconds ) {
+    int limit = clock() + milliseconds * CLOCKS_PER_SEC / 1000;
+        while ( clock() < limit ) {
+                // Do nothing...just wait
+        }
+}
+
+int main() {
+        char ESC = 27;
+        int count = 0;
+        char color = ' ';
+        bool complete = false;
+
+        cout << ESC << "[2J"; // Clear Screen
+        cout << ESC << "[13;0f"; // Move cursor to 13, 0
+        cout << ESC << "[40m";
+        for(int k = 0; k < 8; k++) {
+                for(int j = 0; j < 159; j++) {
+                        for(int i = 0; i < 80; i++) {
+                                if (i == count) {
+                                        color = '0' + k;
+                                        cout << ESC << "[3" << color << "m" << flush;
+                                        cout << "O" << flush;
+                                }
+                                else {
+                                        cout << ESC << "[39m" << flush;
+                                        cout << "o" << flush;
+                                }
+                        }
+                        if (j > 78)
+                                complete = true;
+                        if (!complete)
+                                count++;
+                        else
+                                count--;
+                        cout << ESC << "[13;0f" << flush; // Move cursor to 13, 0
+                        pause(25);
+                }
+                complete = false;
+                count = 0;
+        }
+
+        cout << endl;
+        cout << ESC << "[39;49m";
+        return 0;
+}
