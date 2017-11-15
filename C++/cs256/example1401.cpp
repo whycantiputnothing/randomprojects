cb95cbb4137cbba832e8ca25e0811bce0a5be73c
Brandon Nguyen <happytreefriends001@yahoo.com>
2017-11-15 11:51:29 -0800

    medians of medians

--- C++/cs256/example1401.cpp
+++ C++/cs256/example1401.cpp
@@ -6,44 +6,42 @@ long* reverse(long*, int);
 void print(long*, int);
 
 int main() {
-  long *list;
-  long *revlist;
+	long *list;
+	long *revlist;
 
-  list = fill(100);
-  revlist = reverse(list, 100);
+	list = fill(100);
+	revlist = reverse(list, 100);
 // print(list, 100);
-  print(revlist, 100);
-  cout << sizeof(x) << endl;
-  cout << sizeof(y) << endl;
-  return 0;
+
+	return 0;
 }
 
 long* fill(int n) {
-  long* list = new long[n];
-  list[0] = 0;
-  if (n > 2)
-    list[1] = 1;
+	long* list = new long[n];
+	list[0] = 0;
+	if (n > 2)
+		list[1] = 1;
 
-  for (int i = 2; i < n; i++) {
-    list[i] = list[i-1] + list[i-2];
-  }
+	for (int i = 2; i < n; i++) {
+		list[i] = list[i-1] + list[i-2];
+	}
 
-  return list;
+	return list;
 
 }
 
 long* reverse(long *list, int n) {
-  long *reverse = new long[n];
-  for (int i = 0; i < n; i++) {
-    reverse[i] = list[n-i-1];
-  }
+	long *reverse = new long[n];
+	for (int i = 0; i < n; i++) {
+		reverse[i] = list[n-i-1];
+	}
 
-  return reverse;
+	return reverse;
 }
 
 void print(long *list, int n){
-  for (int i = 0; i < n; i++) {
-    cout << "#" << i + 1 << ": " << list[i] << endl;
-  }
+	for (int i = 0; i < n; i++) {
+		cout << "#" << i + 1 << ": " << list[i] << endl;
+	}
 
 }
---
+++ Java/cs331/MedianOfMedians/MediansOfMedians.java
@@ -0,0 +1,66 @@
+package MedianOfMedians;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class MediansOfMedians {
+
+    public static List<Integer> randomList(int size) {
+        List<Integer> lst = new ArrayList<Integer>();
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            lst.add(rand.nextInt(Integer.MAX_VALUE));
+        }
+        return lst;
+    }
+    
+    public static List<Integer> randomList(int size, int max) {
+        List<Integer> lst = new ArrayList<Integer>();
+        Random rand = new Random();
+        for (int i = 0; i < size; i++) {
+            lst.add(rand.nextInt(max));
+        }
+        return lst;
+    }
+    
+    public static int partition(List<Integer> list, int parition){
+        int pos = 0;
+        for(int i = 0; i < list.size(); i++){
+            if (list.get(i) > parition) {
+                
+            }
+        }
+        return pos;
+    }
+
+    public static int getMedian(List<Integer> list, int k) {
+        if (list.size() <= 5) {
+            list.sort(null);
+            return list.get(k - 1);
+        }
+
+        List<Integer> superMedianList = new ArrayList<Integer>();
+        
+        for(int i = 0; i < list.size()/5; i++) {
+            if (i*5 > list.size()) {
+                superMedianList.add(getMedian(list.subList(i*5, i*5+5), list.size()%5));
+            }
+            else{
+                superMedianList.add(getMedian(list.subList(i*5, i*5+5), 3));                
+            }
+        }
+        int superMedian = getMedian(superMedianList, superMedianList.size()/2);
+        return superMedian;
+    }
+
+    public static void main(String[] args) {
+        List list = randomList(25, 100);
+        System.out.println(list);
+        System.out.println(getMedian(list, 25/2+1));
+        list.sort(null);
+        System.out.println(list);
+    }
+
+}
